34d8d2e4f74d84a0c168c9507c465223f236ebf1
tarslan95 <isimsizbiradres@hotmail.com>
2014-11-03 22:52:38 +0200

    para-yatırma

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 /*PAU Özgür Yazılım Topluluğunun git &amp; Github eğitimi Banka Otomasyonu projesi.*/
 #include <stdio.h>
-
+#include <stdlib.h>
 /*Prototipler*/
 void Menu();
+int para_yatirma(int bakiye);
 
 /*main fonksiyonu*/
 int main() {
@@ -12,4 +13,42 @@ int main() {
 /*Projenin menusu*/
 void Menu() {
 	/*code*/
+}
+
+
+int para_yatirma(int bakiye)
+{
+	int yatirilan_para,dogrulama,istek;
+	system ("cls");
+	printf("Lutfen yatiracaginiz para miktarini giriniz ve onaylayiniz:");
+	scanf("%d",&yatirilan_para);
+	printf("\nParayi yatiriniz.");
+	printf("Yatirdiginiz para miktarini dogrulayiniz.\n");
+	printf("Yatirilan para:%d\n",yatirilan_para);
+	printf("Miktar dogruysa 1 tusuna basin.\n");
+	printf("Miktar yanlissa 1 dısında baska bir  tusa basin.\n");
+	scanf("%d",&dogrulama);
+
+	switch (dogrulama)
+	{
+	case 1:
+		bakiye=bakiye+yatirilan_para;	
+		break;
+	case 2:
+		printf("Miktari tekrar girmek icin 1 tusuna basiniz.\n");
+		printf("Menuye geri donmek icin 1 dısında baska bir tusa basiniz.");
+		scanf("%d",&istek);
+		if (istek==1)
+		{
+			para_yatirma(bakiye);
+		}else 
+		{
+			Menu();
+		}
+		break;
+
+		return bakiye;
+	}
+
+
 }
